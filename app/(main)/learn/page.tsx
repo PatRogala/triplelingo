1e8ad861ed458ecd34de8a1fa32787523c72b71d
Patryk Rogala <rogala.patryk@pm.me>
2024-03-17 16:01:57 +0100

    Add LearnPage component and dependencies, create MobileHeader and Sidebar components, and update MainLayout and globals.css

---
+++ app/(main)/learn/page.tsx
@@ -0,0 +1,10 @@
+const LearnPage = () => {
+  return (
+    <div>
+      <h1>Learn</h1>
+      <p>This is the learn page</p>
+    </div>
+  );
+}
+
+export default LearnPage;
---
+++ components/mobile-header.tsx
@@ -0,0 +1,9 @@
+import { MobileSidebar } from './mobile-sidebar';
+
+export const MobileHeader = () => {
+  return (
+    <nav className="lg:hidden bg-green-500 px-6 h-[50px] flex items-center border-b fixed top-0 w-full z-50">
+      <MobileSidebar />
+    </nav>
+  );
+}
