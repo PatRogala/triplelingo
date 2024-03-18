a0ab3f3861c72734ad2d6c92e25bca6b8304bfea
Patryk Rogala <rogala.patryk@pm.me>
2024-03-18 10:29:28 +0100

    Add component wrappers and update layout

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,8 +1,22 @@
+import { FeedWrapper } from "@/components/feed-wrapper";
+import { StickyWrapper } from "@/components/sticky-wrapper";
+import { Header } from "./header";
+import { UserProgress } from "@/components/user-progress";
+
 const LearnPage = () => {
   return (
-    <div>
-      <h1>Learn</h1>
-      <p>This is the learn page</p>
+    <div className="flex flex-row-reverse gap-[48px] px-6">
+      <StickyWrapper>
+        <UserProgress
+          activeCourse={{ title: "Angielski", imageSrc: "/us.svg" }}
+          hearts={5}
+          points={100}
+          hasActiveSubscription={false}
+        />
+      </StickyWrapper>
+      <FeedWrapper>
+        <Header title="Angielski" />
+      </FeedWrapper>
     </div>
   );
 }
